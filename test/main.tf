65395f462c61db47edbc76010d11328d2aa33699
shakti-garg <shakti.garg@gmail.com>
2020-02-05 18:41:51 +0530

    create key-pair in test instead of hardcoding

--- test/main.tf
+++ test/main.tf
@@ -14,6 +14,11 @@ module "vpc" {
   }
 }
 
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "test-dev"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtHFN/OhAvJtSM4B0wo2NLGkcawfCwL+48nP1p5756eNZ3/o7sd0nz0RPUvHG3CZI6MBdrm/ZmDphDQO3Bm3DpCNQHdtF1IDvpe/zpwtOlJWJTZA2O52x96CfI6OIIi664s5ojYRJQ/8Tks4cyzEE3dFLSo+t2izMvxzPuBGjWlLI+1eeouWKgwUNZp1udkfSROpD2W9BWiKbNWKoKV6zWR/gq3UrTO6qwn+QKzs39ioSUnCGyRn9CfhDw2iN/H/inV8F7/tt1jvryZIYXkkFOb6YPBLw8BwhtZg0jPCdCzS0tVOaC/ybzCYD6vKtH22ak4Y+Xc0EpLsdZ4rjdOz0/ shaktig@INshaktig.local"
+}
+
 module "kafka_cluster" {
   source  = "../"
 
@@ -21,7 +26,7 @@ module "kafka_cluster" {
   vpc_id  = "${module.vpc.vpc_id}"
   vpc_subnet_ids  = "${module.vpc.public_subnets}"
   whitelist_cidrs  = ["223.179.152.208/32","14.142.34.138/32"]
-  keypair_name  = "shakti"
+  keypair_name  = "${aws_key_pair.ssh_key.key_name}"
   num_brokers = 2
   broker_node_linux_ami = "ami-0b898040803850657"
   broker_node_instancetype = "t3.medium"
